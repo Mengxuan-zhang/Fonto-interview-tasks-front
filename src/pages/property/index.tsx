77848d7f62de4857f313b6a12f8a84073eb2287b
ZMX <mengxuanzhangvip@gmail.com>
2022-03-06 16:26:23 +1000

    display data in table

--- src/pages/property/index.tsx
+++ src/pages/property/index.tsx
@@ -10,134 +10,109 @@ import { Table, Tag, Space, Input, Popconfirm, Button } from 'antd';
 import Highlighter from 'react-highlight-words';
 import { SearchOutlined } from '@ant-design/icons';
 import { connect, Dispatch, Loading, PropertyState } from 'umi';
-// import UserModal from './components/UserModal';
 import { ISinglePropertyType, FromValues } from '@/interface/property';
+import { ColumnsType } from 'antd/es/table';
 
 import styles from './index.less';
 
-interface UserPageProps {
-  property: ISinglePropertyType;
+interface PropertyPageProps {
+  property: PropertyState;
   dispatch: Dispatch;
-  // usersDataLoading: boolean,
+  propertyDataLoading: boolean;
 }
 
-const PropertyListPage: FC<UserPageProps> = ({ property, dispatch }) => {
-  const dataSource = [
+const PropertyListPage: FC<PropertyPageProps> = ({
+  property,
+  dispatch,
+  propertyDataLoading,
+}) => {
+  const [totalValuation, setTotalValuation] = useState(0);
+  const columns: ColumnsType<ISinglePropertyType> = [
     {
-      key: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park',
+      title: 'ID',
+      dataIndex: 'id',
+      // filters: [
+      //   {
+      //     text: 'Joe',
+      //     value: 'Joe',
+      //   },
+      //   {
+      //     text: 'Category 1',
+      //     value: 'Category 1',
+      //     // children: [
+      //     //   {
+      //     //     text: 'Yellow',
+      //     //     value: 'Yellow',
+      //     //   },
+      //     //   {
+      //     //     text: 'Pink',
+      //     //     value: 'Pink',
+      //     //   },
+      //     // ],
+      //   },
+      //   {
+      //     text: 'Category 2',
+      //     value: 'Category 2',
+      //     // children: [
+      //     //   {
+      //     //     text: 'Green',
+      //     //     value: 'Green',
+      //     //   },
+      //     //   {
+      //     //     text: 'Black',
+      //     //     value: 'Black',
+      //     //   },
+      //     // ],
+      //   },
+      // ],
+      // filterMode: 'tree',
+      // filterSearch: true,
+      // onFilter: (value, record) => record.name.includes(value),
+      width: '10%',
+      defaultSortOrder: 'ascend',
+      sortDirections: ['ascend', 'descend', 'ascend'],
+      sorter: (a: { id: number }, b: { id: number }) => a.id - b.id,
     },
     {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park',
-    },
-    {
-      key: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sidney No. 1 Lake Park',
-    },
-    {
-      key: '4',
-      name: 'Jim Red',
-      age: 32,
-      address: 'London No. 2 Lake Park',
-    },
-  ];
-
-  const columns = [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      filters: [
-        {
-          text: 'Joe',
-          value: 'Joe',
-        },
-        {
-          text: 'Category 1',
-          value: 'Category 1',
-          // children: [
-          //   {
-          //     text: 'Yellow',
-          //     value: 'Yellow',
-          //   },
-          //   {
-          //     text: 'Pink',
-          //     value: 'Pink',
-          //   },
-          // ],
-        },
-        {
-          text: 'Category 2',
-          value: 'Category 2',
-          // children: [
-          //   {
-          //     text: 'Green',
-          //     value: 'Green',
-          //   },
-          //   {
-          //     text: 'Black',
-          //     value: 'Black',
-          //   },
-          // ],
-        },
-      ],
-      filterMode: 'tree',
-      filterSearch: true,
-      onFilter: (value, record) => record.name.includes(value),
-      width: '30%',
-    },
-    {
-      title: 'Age',
-      dataIndex: 'age',
-      sorter: (a, b) => a.age - b.age,
+      title: 'Valuation',
+      dataIndex: 'valuation',
+      sorter: (a: { valuation: number }, b: { valuation: number }) =>
+        a.valuation - b.valuation,
+      width: '40%',
     },
     {
       title: 'Address',
       dataIndex: 'address',
-      filters: [
-        {
-          text: 'London',
-          value: 'London',
-        },
-        {
-          text: 'New York',
-          value: 'New York',
-        },
-      ],
-      onFilter: (value, record) => record.address.startsWith(value),
       filterSearch: true,
       width: '40%',
     },
   ];
 
-  function onChange(pagination, filters, sorter, extra) {
-    console.log('params', pagination, filters, sorter, extra);
+  function propertyListFilter(properties: ISinglePropertyType[]) {
+    console.log(properties);
+    return properties.length > 0
+      ? properties.filter((property) => property.hidden === false)
+      : [];
   }
-
-  // useEffect(() => {
-  //   console.log(2);
-  //   dispatch({
-  //     type: 'property/getAllList'
-  //   });
-  // }, [])
 
   return (
     <div className={styles['list-table']}>
-      <Table columns={columns} dataSource={dataSource} onChange={onChange} />
+      <Table
+        columns={columns}
+        dataSource={propertyListFilter(property.properties)}
+        rowKey="id"
+        loading={propertyDataLoading}
+      />
+      <p></p>
     </div>
   );
 };
 
-export default connect(({ property }: { property: PropertyState }) => {
-  // console.log(loading)
-  return {
-    property,
-    // usersDataLoading: loading.models.users,
-  };
-})(PropertyListPage);
+export default connect(
+  ({ property, loading }: { property: PropertyState; loading: Loading }) => {
+    return {
+      property,
+      propertyDataLoading: loading.models.property,
+    };
+  },
+)(PropertyListPage);
